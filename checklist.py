77655a379287fd383f65404f35df6528c7a8caf9
John Platkowski <johnjplatkowski@gmail.com>
2024-11-10 16:16:42 -0600

    Update checklist.py

--- checklist.py
+++ checklist.py
@@ -4,13 +4,22 @@
 import random
 import math
 import json
+from datetime import datetime
+employee_data = json.load(open("employee_data.json"))
+employee_list = list(employee_data.keys())
 
 def update_employee_data(ID):
-    # Updates employee data with what tasks were done and when
-    pass
+    # Updates employee data with what tasks were done and when they were done
+    for task in employee_data[ID]["tasks"]:
+        if task["completed"]:
+            task["completed_date"] = datetime.now().strftime("%m/%d/%Y")
+        else:
+            task["completed_date"] = ""
+
 
 
 def main():
-    pass
+    for ID in employee_list:
+        update_employee_data(ID)
 
 main()
